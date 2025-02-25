d2373f34c031fbe562ab18e851ed4379ec6526c6
Sushancode <kumarineha090806@gmail.com>
2025-02-25 12:14:40 -0500

    fixed errors

--- CalculateArea/Calculate_Area.js
+++ CalculateArea/Calculate_Area.js
@@ -1,10 +1,10 @@
 let length;
 let width;
 
-function calculateArea() {
+function CalculateArea() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
    
    let area = length * width; 
+   document.getElementById("result").innerText = `The area of your rectange is: ${area}`;
 }
-document.getElementById("result").innerText = `The area of your rectange is: ${area}`;
