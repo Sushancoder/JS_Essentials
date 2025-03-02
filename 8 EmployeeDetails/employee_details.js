d1ef0bfcaf86a69d874f0d5db0c1ad75280ae994
Sushan <kumarineha090806@gmail.com>
2025-03-02 12:03:31 -0500

    Added specialisation filter

--- 8 EmployeeDetails/employee_details.js
+++ 8 EmployeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialisation:"JavaScript"},
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialisation:"Python" },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialisation:"Rust"},
     //... More employee records can be added here
   ];
 
@@ -10,6 +10,7 @@ function displayEmployees(){
 const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
     document.getElementById('employeesDetails').innerHTML = totalEmployees;
 }
+displayEmployees()
 function calculateTotalSalaries() {
     const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
     alert(`Total Salaries: $${totalSalaries}`);
@@ -30,3 +31,14 @@ function findEmployeeById(employeeId) {
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
 
     }}
+
+
+    function specialisation(lang) {
+        const foundEmployee = employees.find(employee => employee.specialisation === lang);
+        if (foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+        }
+        else{
+          document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this specialisation';
+    
+        }}
